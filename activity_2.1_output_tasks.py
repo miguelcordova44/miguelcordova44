476277cbafcf5231c4ac9d4573fcf5eb8146dc3e
miguelcordova44 <0cordm84@solent.ac.uk>
2025-02-23 11:08:25 +0000

    Updated code to display a longer multi-line message

---
+++ activity_2.1_output_tasks.py
@@ -0,0 +1,9 @@
+# Display message to the screen
+print("Welcome to QHO426!")
+print("In week 1 we will learn...")
+print()
+print("...How to use Git and GitHub")
+print("...How to output to the screen")
+print("...How to get user input")
+print()
+print("I hope you are enjoying the lesson thus far!")
